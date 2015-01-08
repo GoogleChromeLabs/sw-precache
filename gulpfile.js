73a5d7a595e0bbb6976cf1c345b6a3c78bd835ad
Jeff Posnick <jeffy@google.com>
2015-01-08 11:13:38 -0500

    Wrap the fetch handler's logic in a top-level if() so that it only triggers if we're fetching a URL we have pre-cached.

--- gulpfile.js
+++ gulpfile.js
@@ -28,7 +28,7 @@ function runExpress(port, rootDir) {
 function generateServiceWorkerFileContents(rootDir, handleFetch) {
   return swPrecache({
     dynamicUrlToDependencies: {
-      'dynamic/page1': [rootDir + '/views/layout.jade', rootDir + '/views/page1.jade'],
+      //'dynamic/page1': [rootDir + '/views/layout.jade', rootDir + '/views/page1.jade'],
       'dynamic/page2': [rootDir + '/views/layout.jade', rootDir + '/views/page2.jade']
     },
     handleFetch: handleFetch,
