d75659c93c5de4f85f2a4a1e88ab28b067602ace
Jeff Posnick <jeffy@google.com>
2015-01-26 11:01:22 -0500

    Created a top-level gulpfile to handle common module tasks.

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
+var runSequence = require('run-sequence');
+var spawn = require('child_process').spawn;
+
+gulp.task('default', ['test']);
+
+gulp.task('test', function() {
+  return gulp.src('test/*.js', {read: false})
+    .pipe($.mocha())
+    .on('error', process.exit);
+});
+
+gulp.task('publish', ['test'], function(callback) {
+  spawn('npm', ['publish'], {stdio: 'inherit'})
+    .on('close', callback);
+});
+
+gulp.task('gh-pages', function(callback) {
+  spawn('gulp', ['--cwd', 'demo', 'gh-pages'], {stdio: 'inherit'})
+    .on('close', callback);
+});
+
+gulp.task('release', function(callback) {
+  runSequence('publish', 'gh-pages', callback);
+});
