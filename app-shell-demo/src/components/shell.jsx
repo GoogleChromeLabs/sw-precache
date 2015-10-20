3b7b12244c90c52e467bef31e9343981895e94b6
Jeff Posnick <jeffy@google.com>
2015-10-20 16:16:48 -0400

    Properly name the Shell class

--- app-shell-demo/src/components/shell.jsx
+++ app-shell-demo/src/components/shell.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default class Hello extends React.Component {
+export default class Shell extends React.Component {
   render() {
     return <span></span>;
   }
