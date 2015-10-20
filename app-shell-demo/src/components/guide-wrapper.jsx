4e966314ca7be4eedf40d9c0d91191dfa59a793b
Jeff Posnick <jeffy@google.com>
2015-10-20 15:59:42 -0400

    Some basic guide parsing.

--- app-shell-demo/src/components/guide-wrapper.jsx
+++ app-shell-demo/src/components/guide-wrapper.jsx
@@ -2,12 +2,17 @@ import React from 'react';
 
 export default class GuidesWrapper extends React.Component {
   render() {
-    let guide = this.props.guide || {};
+    let guide = this.props.guide;
+    if (!guide) {
+      return <h3>Loading...</h3>;
+    }
 
     return (
-      <ul>
-        {JSON.stringify(guide)}
-      </ul>
+      <div>
+        <h1>{guide.get('title')}</h1>
+        <h3>By {guide.get('author').get('username')}</h3>
+        <h4>Last updated on {new Date(guide.get('modified_date') * 1000).toDateString()}</h4>
+      </div>
     );
   }
 }
