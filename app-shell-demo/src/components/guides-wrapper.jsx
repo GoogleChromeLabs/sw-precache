331fd59acf0730b3c89c158086ee3dfae6b07ebe
Jeff Posnick <jeffy@google.com>
2015-10-22 14:25:30 -0400

    Some initial styling

--- app-shell-demo/src/components/guides-wrapper.jsx
+++ app-shell-demo/src/components/guides-wrapper.jsx
@@ -3,7 +3,11 @@ import {Link} from 'react-router';
 
 class GuidesItemWrapper extends React.Component {
   render() {
-    return <li><Link to={this.props.to}>{this.props.title}</Link></li>;
+    return (
+      <div className="card">
+        <Link to={this.props.to} title={this.props.title}><img src={this.props.image}/></Link>
+      </div>
+    );
   }
 }
 
@@ -13,13 +17,14 @@ export default class GuidesWrapper extends React.Component {
     let guides = this.props.guides || [];
 
     return (
-      <ul>
+      <div className="card-container">
         {guides.map(guide => {
           return <GuidesItemWrapper key={guide.get('guideid')}
-                                   to={GUIDE_PREFIX + guide.get('guideid')}
-                                   title={guide.get('title')}/>;
+                                    to={GUIDE_PREFIX + guide.get('guideid')}
+                                    image={guide.get('image').get('standard')}
+                                    title={guide.get('title')}/>;
         })}
-      </ul>
+      </div>
     );
   }
 }
--- app-shell-demo/src/components/header.jsx
+++ app-shell-demo/src/components/header.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default class Footer extends React.Component {
   render() {
-    return <h1>The Web App</h1>;
+    return <h1>iFixit Progressive Web App</h1>;
   }
 }
