28136db4742750cc7a8f585753e7a40ddea9f12f
Kevin Phelps <kevinphelps@users.noreply.github.com>
2016-12-14 12:09:57 -0600

    fix: set navigateFallbackWhitelist from config (#227)

--- cli.js
+++ cli.js
@@ -56,6 +56,9 @@ function setDefaults(cli, configFileFlags) {
   compositeFlags.navigateFallback = compositeFlags.navigateFallback ||
     configFileFlags.navigateFallback;
 
+  compositeFlags.navigateFallbackWhitelist = compositeFlags.navigateFallbackWhitelist ||
+    configFileFlags.navigateFallbackWhitelist;
+
   compositeFlags.staticFileGlobs = compositeFlags.staticFileGlobs ||
     configFileFlags.staticFileGlobs;
   if (compositeFlags.staticFileGlobs) {
